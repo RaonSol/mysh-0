a84b7651a770a7dcf44ac0550e86a0be5ecd9010
RaonSol <windows0903@gmail.com>
2017-10-10 01:45:56 +0900

    Fill the blank

--- src/commands.c
+++ src/commands.c
@@ -14,18 +14,18 @@ int do_pwd(int argc, char** argv) {
     if (!validate_pwd_argv(argc, argv))
         return -1;
     
-    char pwd[8096] = {};
+    char pwd[8096]={'\0'};
     getcwd(pwd,8096);
     printf("%s\n",pwd);
     return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-    if(strcmp(argv[0],"cd")==0 && argc == 2) return 1;
+    if(strcmp(argv[0], "cd")==0 && argc==2) return 1;
     else return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-    if(strcmp(argv[0],"pwd")==0 && argc == 1) return 1;
+    if(strcmp(argv[0], "pwd")==0 && argc==1) return 1;
     else return 0;
 }
