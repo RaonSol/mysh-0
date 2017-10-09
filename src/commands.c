f506875a28cd8551ecba33eff56ce26386c1613d
RaonSol <windows0903@gmail.com>
2017-10-10 01:27:08 +0900

    Fill the blank

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,31 @@
 #include <string.h>
-
+#include <unistd.h>
+#include <stdio.h>
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
-  if (!validate_cd_argv(argc, argv))
-    return -1;
-
-  // TODO: Fill it!
-  return 0;
+    if (!validate_cd_argv(argc, argv))
+        return -1;
+    chdir(argv[1]);
+    return 0;
 }
 
 int do_pwd(int argc, char** argv) {
-  if (!validate_pwd_argv(argc, argv))
-    return -1;
-
-  // TODO: Fill it!
-
-  return 0;
+    if (!validate_pwd_argv(argc, argv))
+        return -1;
+    
+    char pwd[8096] = {};
+    getcwd(pwd,8096);
+    printf("%s\n",pwd);
+    return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+    if(strcmp(argv[0],"cd")==0 && argc == 2) return 1;
+    else return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+    if(strcmp(argv[0],"pwd")==0 && argc == 1) return 1;
+    else return 0;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,29 @@
+#include <string.h>
+#include <stdlib.h>
 #include "utils.h"
 
-void mysh_parse_command(const char* command,
-                        int *argc, char*** argv)
+void mysh_parse_command(const char* command, int *argc, char*** argv)
 {
-  // TODO: Fill this!
+    int idx=0;
+    char cmdCpy[8096]={'\0'};
+    char *parTmp;
+    strcpy(cmdCpy, command);
+    *argv=(char**)malloc(sizeof(char*));
+    (*argv)[0]=(char*)malloc(sizeof(char));
+    
+    for(int i=0;cmdCpy[i]!='\0';i++)
+        if(cmdCpy[i]=='\n'||cmdCpy[i]=='\t') cmdCpy[i]=' ';
+    
+    parTmp=strtok(cmdCpy, " ");
+    for(idx=0;parTmp!=0;idx++)
+    {
+        *argv=(char**)realloc(*argv,sizeof(char*)*(idx+1));
+        int size=(int)strlen(parTmp);
+        (*argv)[idx]=(char*)malloc(sizeof(char)*(size+1));
+        strcpy((*argv)[idx],parTmp);
+        parTmp = strtok(NULL," ");
+    }
+    *argc=idx;
+    
+    if(**argv==NULL) strcpy((*argv)[0],"");
 }
